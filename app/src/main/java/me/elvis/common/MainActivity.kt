d9e19dc2338c36dea44b0f0c40bcc6c74c93e721
elvis <xzttt5y@gmail.com>
2019-07-31 17:33:58 +0800

    add shadowLayout

--- app/src/main/java/me/elvis/common/MainActivity.kt
+++ app/src/main/java/me/elvis/common/MainActivity.kt
@@ -4,7 +4,6 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.zhy.adapter.recyclerview.CommonAdapter
 import com.zhy.adapter.recyclerview.base.ViewHolder
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,7 +12,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        list.adapter=MyAdapter()
     }
 
     inner class MyAdapter : CommonAdapter<String>(this@MainActivity, R.layout.item, datas) {
